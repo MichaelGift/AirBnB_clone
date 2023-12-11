c68a23045e830a32c9ca456a7e1ea7e056438932
MichaelGift <simiyugift@outlook.com>
2023-12-11 19:52:49 +0300

    Add feature to save data

--- models/base_model.py
+++ models/base_model.py
@@ -4,19 +4,33 @@
 from uuid import uuid4
 from datetime import datetime
 
+import models
+
 
 class BaseModel:
     """The blueprint for all other classes"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize a new BaseModel."""
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(
+                        value, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key == "__class__":
+                    setattr(self, key, type(self))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.today()
+            self.updated_at = datetime.today()
+            models.storage.new(self)
 
     def save(self):
         """Update the updated_at attribute to the current date and time"""
         self.updated_at = datetime.today()
+        models.storage.save()
 
     def to_dict(self):
         """Return the dictionary representation of the BaseModel instance."""
